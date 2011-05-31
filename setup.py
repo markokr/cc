5b827a95e208b55142c966a2acc03f7f5d9cb842
Marko Kreen <markokr@gmail.com>
2011-05-31 13:33:59 +0300

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name = "cc",
+    version = '0.1',
+    maintainer = "Marko Kreen",
+    maintainer_email = "marko.kreen@skype.net",
+    packages = ['cc', 'hots'],
+    scripts = [
+        'bin/ccserver.py',
+        'bin/cctaskrunner.py',
+        'bin/infosender.py',
+    ],
+)
+
