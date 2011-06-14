afc46b9b680101ae0cffbfb9e8c468bb2937ce06
Marko Kreen <markokr@gmail.com>
2011-06-14 13:14:40 +0300

    jobmgr: collect daemon output during startup

--- cc/handler/jobmgr.py
+++ cc/handler/jobmgr.py
@@ -1,6 +1,7 @@
 
-import os, subprocess
+import os, subprocess, select, fcntl
 
+from zmq.eventloop.ioloop import PeriodicCallback
 from cc.handler import CCHandler
 
 import skytools
@@ -9,14 +10,96 @@ __all__ = ['JobMgr']
 
 CC_HANDLER = 'JobMgr'
 
+def set_nonblocking(fd, onoff):
+    """Toggle the O_NONBLOCK flag.
+    If onoff==None then return current setting.
+    """
+    flags = fcntl.fcntl(fd, fcntl.F_GETFL)
+    if onoff is None:
+        return (flags & os.O_NONBLOCK) > 0
+    if onoff:
+        flags |= os.O_NONBLOCK
+    else:
+        flags &= ~os.O_NONBLOCK
+    fcntl.fcntl(fd, fcntl.F_SETFL, flags)
+
+def set_cloexec(fd, onoff):
+    """Toggle the FD_CLOEXEC flag.
+    If onoff==None then return current setting.
+    """
+    flags = fcntl.fcntl(fd, fcntl.F_GETFD)
+    if onoff is None:
+        return (flags & fcntl.FD_CLOEXEC) > 0
+    if onoff:
+        flags |= fcntl.FD_CLOEXEC
+    else:
+        flags &= ~fcntl.FD_CLOEXEC
+    fcntl.fcntl(fd, fcntl.F_SETFL, flags)
+
 #
 # JobMgr
 #
 
 class JobState:
-    def __init__(self, jname, jcf):
+    def __init__(self, jname, jcf, log, cc_url, ioloop):
         self.jname = jname
         self.jcf = jcf
+        self.proc = None
+        self.log = log
+        self.cc_url = cc_url
+        self.timer = None
+        self.ioloop = ioloop
+        pidfiledir = self.jcf.getfile('pidfiledir', '~/pid')
+        self.pidfile = "%s/%s.pid" % (pidfiledir, self.jname)
+        self.cfdict = {
+                'job_name': self.jname,
+                'pidfile': self.pidfile,
+        }
+        for o in self.jcf.options():
+            self.cfdict[o] = self.jcf.get(o)
+
+    def handle_timer(self):
+        if self.proc:
+            self.log.info('JobState.handle_timer')
+            data = self.proc.stdout.read()
+            if data:
+                self.log.info('handle_timer: stdout=%s', repr(data.strip()))
+            if self.proc.poll() is not None:
+                x = self.proc.wait()
+                self.log.info('handle_timer: proc exited with %s', repr(x))
+                self.proc = None
+        else:
+            # daemonization successful?
+            live = skytools.signal_pidfile(self.pidfile, 0)
+            if live:
+                self.log.info('handle_timer: %s is alive', self.jname)
+            else:
+                self.log.info('handle_timer: %s is dead', self.jname)
+
+
+    def start(self):
+        # unsure about the best way to specify target
+        mod = self.jcf.get('module', '')
+        script = self.jcf.get('module', '')
+        cls = self.jcf.get('class', '')
+        args = ['-d', '--cc', self.cc_url, '--ccdaemon', self.jname]
+        if mod:
+            cmd = ['python', '-m', mod] + args
+        elif script:
+            cmd = [script] + args
+        else:
+            raise skytools.UsageError('dunno how to launch class')
+
+        self.log.info('Launching %s: %s', self.jname, cmd)
+        self.proc = subprocess.Popen(cmd, close_fds=True,
+                                stdin = open(os.devnull, 'rb'),
+                                stdout = subprocess.PIPE,
+                                stderr = subprocess.STDOUT)
+
+        set_nonblocking(self.proc.stdout, True)
+
+        self.timer = PeriodicCallback(self.handle_timer, 2*1000, self.ioloop)
+        self.timer.start()
 
 class JobMgr(CCHandler):
     """Provide config to local daemons / tasks."""
@@ -32,33 +115,10 @@ class JobMgr(CCHandler):
 
     def add_job(self, jname):
         jcf = skytools.Config(jname, self.cf.filename, ignore_defs = True)
-        self.jobs[jname] = JobState(jname, jcf)
+        jstate = JobState(jname, jcf, self.log, self.local_url, self.ioloop)
+        self.jobs[jname] = jstate
+        jstate.start()
         
-        # unsure about the best way to specify target
-        mod = jcf.get('module', '')
-        script = jcf.get('module', '')
-        cls = jcf.get('class', '')
-        args = ['-d', '--cc', self.local_url, '--ccdaemon', jname]
-        if mod:
-            cmd = ['python', '-m', mod] + args
-        elif script:
-            cmd = [script] + args
-        else:
-            raise skytools.UsageError('dunno how to launch class')
-
-        self.log.info('Launching %s: %s', jname, cmd)
-        p = subprocess.Popen(cmd, close_fds=True,
-                                stdin = open(os.devnull, 'rb'),
-                                stdout = subprocess.PIPE,
-                                stderr = subprocess.STDOUT)
-        outbuf = p.communicate()[0]
-        if p.returncode != 0:
-            self.log.error('Daemon launch failed (%s, exitcode=%d): %s', jname, p.returncode, outbuf.strip())
-        elif outbuf:
-            self.log.warning('Noisy daemon startup: [%s], %s', jname, repr(outbuf.strip()))
-        else:
-            self.log.info('Daemon launch successful: %s', jname)
-
     def handle_msg(self, cmsg):
         """Got message from client, send to remote CC"""
 
@@ -67,18 +127,9 @@ class JobMgr(CCHandler):
 
         res = {'req': data['req']}
         if data['req'] == 'job.config':
-            # fill defaults
+            # send config
             job = self.jobs[data['job_name']]
-            cf = {
-                    'job_name': data['job_name'],
-                    'pidfiledir': self.cf.getfile('pidfiledir', '~/pid'),
-                    'pidfile': '%(pidfiledir)s/%(job_name)s.pid',
-            }
-
-            # move config from cc-s .ini file
-            for o in job.jcf.options():
-                cf[o] = job.jcf.get(o)
-            res['config'] = cf
+            res['config'] = job.cfdict
         else:
             res['msg'] = 'Unsupported req'
         ans = cmsg.make_reply(res)
