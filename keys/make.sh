8e6ff7b249f37d639b4d68b2b047c83255b8fea9
Marko Kreen <markokr@gmail.com>
2012-02-21 17:44:14 +0200

    keys: add second CA

--- keys/make.sh
+++ keys/make.sh
@@ -14,11 +14,17 @@
 rm -f private
 ln -s . private
 rm -rf testca
+rm -rf testca2
 
 mkdir -p testca/private
 mkdir -p testca/newcerts
 touch testca/index.txt
 echo 01 > testca/serial
+
+mkdir -p testca2/private
+mkdir -p testca2/newcerts
+touch testca2/index.txt
+echo 01 > testca2/serial
 
 run() {
   echo '$' "$@"
@@ -56,6 +62,17 @@ run_ca() {
   fi
 }
 
+run_ca2() {
+  ser=`cat testca2/serial`
+  run openssl ca -batch -config ca2.ini "$@"
+  while test "$1" != '-out'; do
+    shift
+  done
+  if test "$1" = '-out'; then
+    cp "testca2/newcerts/$ser.pem" "$2"
+  fi
+}
+
 ksize=2048
 ksize=1024
 ksize=512
@@ -66,12 +83,15 @@ run openssl genrsa -out user1.key $ksize
 
 run openssl genrsa -out ca.key $ksize
 
+run openssl genrsa -out ca2.key $ksize
+
 run openssl genrsa -out server.key $ksize
 
 run openssl genrsa -out confdb.key $ksize
 
 # self-signed cert
-run_req -new -x509 -days $days -key ca.key -out ca.crt -- C=EE L=Tartu O=Skype OU=TempCA CN="Test CA Server"
+run_req -new -x509 -days $days -key ca.key -out ca.crt   -- C=EE L=Tartu O=Skype OU=TempCA CN="Test CA1 Server"
+run_req -new -x509 -days $days -key ca2.key -out ca2.crt -- C=EE L=Tartu O=Skype OU=ServerCA CN="Test CA2 Server"
 
 # cert reqs
 run_req -new -key server.key -out server.csr -- C=EE L=Tartu O=Skype OU=DubColo CN="Host 1"
@@ -79,7 +99,7 @@ run_req -new -key confdb.key -out confdb.csr -- C=EE L=Tartu O=Skype OU=Service 
 run_req -new -key user1.key  -out user1.csr  -- C=EE L=Tartu O=Skype OU=SiteOps CN="User Name" emailAddress="user1@skype.net"
 
 # accept certs
-run_ca -days $days -policy pol-user   -in user1.csr  -out user1.crt
-run_ca -days $days -policy pol-server -in server.csr -out server.crt
-run_ca -days $days -policy pol-server -in confdb.csr -out confdb.crt
+run_ca  -days $days -policy pol-user   -in user1.csr  -out user1.crt
+run_ca2 -days $days -policy pol-server -in server.csr -out server.crt
+run_ca2 -days $days -policy pol-server -in confdb.csr -out confdb.crt
 
