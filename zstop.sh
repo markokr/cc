415fde5b03a0fd95072fec1fe851cab14c12e885
martin otto <martin.otto@skype.net>
2012-08-03 19:13:17 +0200

    zstop.sh: fixed "Double ^C" warning issue

--- zstop.sh
+++ zstop.sh
@@ -2,14 +2,16 @@
 
 #set -e
 
-for f in ~/pid/*.pid; do
-  if test -f "$f"; then
-    echo "stopping $f"
-    kill -s INT "`cat $f`"
-  fi
-done
+#for f in ~/pid/*.pid; do
+#  if test -f "$f"; then
+#    echo "stopping $f"
+#    kill -s INT "`cat $f`"
+#  fi
+#done
 
+echo "stopping cc.daemon's"
 pkill -f -s INT cc.daemon
+echo "stopping cc.server's"
 pkill -f -s INT cc.server
 
 #rm -rf ./var/log ./var/run ./var/info* ./infodir/info*
